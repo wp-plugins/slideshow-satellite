cf65d010977be878400a43e32e2e70fb560f50f2
cpres <cpres@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-01 08:28:08 +0000

    Adding newest version info
    git-svn-id: https://plugins.svn.wordpress.org/slideshow-satellite/branches/2.1@706340 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- slideshow-satellite.php
+++ slideshow-satellite.php
@@ -5,9 +5,9 @@ Plugin URI: http://c-pr.es/projects/satellite
 Author: C- Pres
 Author URI: http://c-pr.es/membership-options
 Description: Responsive display for all your photo needs. Customize to your hearts content.
-Version: 2.1
+Version: 2.1.1
 */
-define( 'SATL_VERSION', '2.1');
+define( 'SATL_VERSION', '2.1.1');
 $uploads = wp_upload_dir();
 if ( ! defined( 'SATL_PLUGIN_BASENAME' ) )
 	define( 'SATL_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );

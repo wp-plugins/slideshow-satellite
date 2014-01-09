c4a5208b105a068feb5cfd1442b198fe98715319
cpres <cpres@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-09 19:20:47 +0000

    prepping 2.2.1 release with #s
    git-svn-id: https://plugins.svn.wordpress.org/slideshow-satellite/branches/2.2@835617 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://c-pr.es/satellite
 Tags: responsive slideshow, photography, watermark, satellite, orbit, responsive, slideshow, infinite scroll, lazy load, flipbook 
 Requires at least: 3.1
 Tested up to: 3.8
-Stable tag: 2.2
+Stable tag: 2.2.1
 License: GPLv2 or later
 
 So Responsive! So Customizable! So Modern! Build Slideshows, Flipbooks, and do Infinite Scrolls with Slideshow Satellite. 
--- slideshow-satellite.php
+++ slideshow-satellite.php
@@ -5,9 +5,9 @@ Plugin URI: http://c-pr.es/satellite
 Author: C- Pres
 Author URI: http://c-pr.es
 Description: Responsive display for all your photo needs. Customize to your hearts content.
-Version: 2.2
+Version: 2.2.1
 */
-define('SATL_VERSION', '2.2');
+define('SATL_VERSION', '2.2.1');
 $uploads = wp_upload_dir();
 if (!defined('SATL_PLUGIN_BASENAME'))
     define('SATL_PLUGIN_BASENAME', plugin_basename(__FILE__));
